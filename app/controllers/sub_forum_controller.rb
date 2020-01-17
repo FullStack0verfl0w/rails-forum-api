f39f506bbc30b4e4804a62795cf81f25d124e8f9
pBinarySoul <pBinarySoul@gmail.com>
2020-01-17 22:49:02 +0300

    Rewrote for new right system

--- app/controllers/sub_forum_controller.rb
+++ app/controllers/sub_forum_controller.rb
@@ -1,78 +1,60 @@
 class SubForumController < ApplicationController
     def index
-        now = Time::now
         token = session[:token]
         status = :ok
 
         # Check token and find user with it
         if token
-            user = User.where("session = ? AND sessionEnd > ?", token, now).first
+            user = User.find_by "token = ? AND tokenEnd > ?", token, Time::now
         end
 
         # Get all records from database
-        begin
-            #                   Compute flag right here
-            data = SubForum.where("rightFlags & ? = ?", User::RightFlags::FORUM_READ, User::RightFlags::FORUM_READ)
+        data = SubForum.all
 
-        # Tell a client something went wrong
-        rescue Exception => e
-            puts e.message
-            status = :internal_server_error
+        # Find client's user group
+        if user
+            group = UserGroup.find_by name: user.userGroup
         else
+            group = UserGroup.find_by name: UserGroup::DefaultGroups[:guest][:name]
+        end
 
-            # Check client's right flag if client is authorized
-            if user
-                user_has_read_flag = User::RightFlags.bit_set(user.rightFlags, User::RightFlags::FORUM_READ)
-
-                # If client has no flag, return nothing
-                if !user_has_read_flag
-                    data = []
-                end
-            end
+        # If that user group has no flag to read we make data array empty
+        if !group.has_flag UserGroup::RightFlags::FORUM_READ
+            data = []
         end
 
         # Regenerate session token
         if user
-            user.session = user.generate_token
-            user.sessionEnd = now + User::SESSION_EXPIRY_TIME
-            user.lastActivityTime = now
-            user.save!
-            session[:token] = user.session
-            session[:expires_at] = user.sessionEnd
+            user.generate_token session
         end
         render json: {data: data}, status: status
     end
 
     def create
-        now = Time::now
         token = session[:token]
         status = :ok
 
         if token
-            user = User.where("session = ? AND sessionEnd > ?", token, now).first
+            user = User.find_by "token = ? AND tokenEnd > ?", token, Time::now
 
-            if user && User::RightFlags.bit_set(user.rightFlags, User::RightFlags::FORUM_CREATE)
+            if user
+                group = UserGroup.find_by name: user.userGroup
+            else
+                group = UserGroup.find_by name: UserGroup::DefaultGroups[:guest][:name]
+            end
+
+            if group.has_flag UserGroup::RightFlags::FORUM_CREATE
                 name = params[:title] || ""
                 desc = params[:desc] || ""
                 icon = params[:icon] || SubForum::ICONS[0]
-                flag = params[:flag] || User::RightFlags::FORUM_READ
+                can_view = params[:canView] || [:all].to_json
 
-                # Find already existing sub forum
-                forum = SubForum.where(name: name).first
+                # Check name and description length
+                if  (name.length < SubForum::TITLE_MIN_LEN && SubForum::TITLE_MIN_LEN != -1) ||
+                    (name.length > SubForum::TITLE_MAX_LEN && SubForum::TITLE_MAX_LEN != -1) ||
+                    (desc.length < SubForum::DESC_MIN_LEN && SubForum::DESC_MIN_LEN != -1) ||
+                    (desc.length > SubForum::DESC_MAX_LEN && SubForum::DESC_MAX_LEN != -1)
 
-                if forum
-                    status = :bad_request
-
-                # Check name length
-                elsif name.length < SubForum::TITLE_MIN_LEN && SubForum::TITLE_MIN_LEN != -1
-                    status = :bad_request
-                elsif name.length > SubForum::TITLE_MAX_LEN && SubForum::TITLE_MAX_LEN != -1
-                    status = :bad_request
-
-                # Check description length
-                elsif desc.length < SubForum::DESC_MIN_LEN && SubForum::DESC_MIN_LEN != -1
-                    status = :bad_request
-                elsif desc.length > SubForum::DESC_MAX_LEN && SubForum::DESC_MAX_LEN != -1
                     status = :bad_request
 
                 # If everything is alright create record in database
@@ -81,19 +63,69 @@ class SubForumController < ApplicationController
                         name: name,
                         description: desc,
                         icon: icon,
-                        rightFlags: flag,
+                        canView: can_view,
                         posts: [].to_json
                     )
                     forum.save!
                 end
 
                 # Regenerate session token
-                user.session = user.generate_token
-                user.sessionEnd = now + User::SESSION_EXPIRY_TIME
-                user.lastActivityTime = now
-                user.save!
-                session[:token] = user.session
-                session[:expires_at] = user.sessionEnd
+                user.generate_token session
+            else
+                status = :unauthorized
+            end
+        else
+            status = :unauthorized
+        end
+        render status: status
+    end
+
+    def update
+        id = params[:id]
+        token = session[:token]
+        status = :ok
+
+        if token
+            user = User.find_by "token = ? AND tokenEnd > ?", token, Time::now
+
+            if user
+                group = UserGroup.find_by name: user.userGroup
+            else
+                group = UserGroup.find_by name: UserGroup::DefaultGroups[:guest][:name]
+            end
+
+            if group.has_flag UserGroup::RightFlags::FORUM_MODIFY
+                name = params[:title] || ""
+                desc = params[:desc] || ""
+                icon = params[:icon] || SubForum::ICONS[0]
+                can_view = params[:canView] || [:all].to_json
+
+                # Find existing sub forum
+                forum = SubForum.find_by id: id
+
+                if forum
+                    # Check name and description length
+                    if (name.length < SubForum::TITLE_MIN_LEN && SubForum::TITLE_MIN_LEN != -1) ||
+                        (name.length > SubForum::TITLE_MAX_LEN && SubForum::TITLE_MAX_LEN != -1) ||
+                        (desc.length < SubForum::DESC_MIN_LEN && SubForum::DESC_MIN_LEN != -1) ||
+                        (desc.length > SubForum::DESC_MAX_LEN && SubForum::DESC_MAX_LEN != -1)
+
+                        status = :bad_request
+
+                    # If everything is alright update record in database
+                    else
+                        forum.name = name
+                        forum.description = desc
+                        forum.icon = icon
+                        forum.canView = can_view
+                        forum.save!
+                    end
+                else
+                    status = :not_found
+                end
+
+                # Regenerate session token
+                user.generate_token session
             else
                 status = :unauthorized
             end
