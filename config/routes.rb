850531437db62d1f2b618c5d2f0ca533afec6dfc
pBinarySoul <pBinarySoul@gmail.com>
2020-01-23 16:07:39 +0300

    Posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :sub_forum
+  get "/sub_forum/:id/posts(.:format)" => "sub_forum#show_subforum_posts"
+  get "/sub_forum/:id/posts/:post" => "sub_forum#show_post"
+  post "/sub_forum/:id/posts/create(.:format)" => "sub_forum#create_post"
+  post "/sub_forum/:id/posts/delete/:post(.:format)" => "sub_forum#destroy_post"
+  post "/sub_forum/:id/posts/upvote/:post" => "sub_forum#upvote_post"
+  post "/sub_forum/:id/posts/downvote/:post" => "sub_forum#downvote_post"
+
   root "application#stub"
 
   post "/auth/steam/callback" => "user#create"
