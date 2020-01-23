3b789fff80b6ef2fbb4f81d4a44ffd9df390f6e9
pBinarySoul <pBinarySoul@gmail.com>
2020-01-23 16:06:21 +0300

    Post Controller module

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,216 @@
-class PostsController < ApplicationController
+# class PostsController < ApplicationController
+# end
+
+module PostsController
+    GET_POSTS_LIMIT = 30 # Maximum posts that server will get from database
+
+    # Show all posts in range (max 30) ordered by creation time. Format: subforum/:id/posts?start=n&end=nn
+    def show_subforum_posts
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_READ) { |status, params, session|
+            id = params[:id].to_i
+            start_pos = params[:start].to_i || GET_POSTS_LIMIT
+            end_pos = params[:end].to_i
+
+            if start_pos > GET_POSTS_LIMIT
+                start_pos = GET_POSTS_LIMIT
+            end
+
+            if !SubForum.exists?(id: id)
+                status = :not_found # Sub Forum doesn't exist
+            else
+                query = if end_pos != 0
+                    Post.escape_sql(["SELECT `posts`.* FROM `posts` WHERE subforum = ? ORDER BY `posts`.`created_at` ASC LIMIT ?, ?", id, start_pos, end_pos])
+                else
+                    Post.escape_sql(["SELECT `posts`.* FROM `posts` WHERE subforum = ? ORDER BY `posts`.`created_at` ASC LIMIT ?", id, start_pos])
+                end
+                data = Post.find_by_sql query
+            end
+
+            [status, data]
+        }
+        render json: {data: data}, status: status
+    end
+
+    def show_post
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_READ) { |status, params, session|
+            id = params[:id].to_i
+            post_id = params[:post].to_i
+
+            post = Post.where("id = ? AND subforum = ?", post_id, id)
+
+            if !SubForum.exists?(id: id) || !post.exists?
+                status = :not_found # Sub Forum or Post doesn't exist
+            end
+
+            [status, post]
+        }
+        render json: {data: data}, status: status
+    end
+
+    def create_post
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_CREATE) { |status, params, session, user|
+            id = params[:id].to_i
+            title = params[:title]
+            content = params[:content]
+            icon = params[:icon]
+
+            if id.blank? || title.blank? || content.blank? || user.blank? || !ApplicationController::Icons.has_value?(icon)
+                status = :bad_request
+            elsif !SubForum.exists?(id: id)
+                status = :not_found # Sub Forum doesn't exist
+            else
+                post = Post.create(
+                    title: title,
+                    content: content,
+                    icon: icon,
+                    subforum: id,
+                    creatorSteamID: user.steamID
+                )
+                post.save!
+
+                forum = SubForum.find id
+                forum_posts = JSON.parse forum.posts
+                forum_posts.push post.id
+                forum.posts = forum_posts.to_json
+                forum.save!
+            end
+
+            [status, data]
+        }
+        render status: status
+    end
+
+    def update_post
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_MODIFY) { |status, params, session, user|
+            id = params[:id].to_i
+            post_id = params[:post].to_i
+            title = params[:title]
+            content = params[:content]
+            icon = params[:icon]
+
+            if id.blank? || title.blank? || content.blank? || user.blank? || !ApplicationController::Icons.has_value?(icon)
+                status = :bad_request
+            elsif !SubForum.exists?(id: id) || !Post.where("id = ? AND subforum = ?", post_id, id).exists?
+                status = :not_found # Sub Forum or Post doesn't exist
+            else
+                post = Post.find(id)
+                post.title = title
+                post.content = content
+                post.icon = icon
+                post.save!
+            end
+
+            [status, data]
+        }
+        render status: status
+    end
+
+    def destroy_post
+        # First check right for deletion your own threads
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_DELETE_OWN) { |status, params, session, user|
+            subforum_id = params[:id].to_i
+            post_id = params[:post].to_i
+
+            if subforum_id.blank? || post_id.blank?
+                status = :bad_request
+            elsif !SubForum.exists?(id: subforum_id)
+                status = :not_found # Sub Forum doesn't exist
+            else
+                begin
+                    post = Post.find_by "id = ? AND subforum = ?", post_id, subforum_id
+                    if post.creatorSteamID == user.steamID
+                        forum = SubForum.find subforum_id
+                        forum_posts = JSON.parse forum.posts
+                        forum_posts.delete post.id
+                        forum.posts = forum_posts.to_json
+                        forum.save!
+
+                        post.destroy
+                    else
+                        status = :unauthorized
+                    end
+                rescue ActiveRecord::RecordNotFound
+                    status = :not_found
+                end
+            end
+
+            [status, data]
+        }
+
+        # If you're not creator or you don't have a right to delete your own threads
+        # We check right for just thread deletion
+        if status == :unauthorized
+            status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::THREAD_DELETE) { |status, params, session|
+                subforum_id = params[:id].to_i
+                post_id = params[:post].to_i
+
+                if subforum_id.blank? || post_id.blank?
+                    status = :bad_request
+                elsif !SubForum.exists?(id: subforum_id)
+                    status = :not_found # Sub Forum doesn't exist
+                else
+                    begin
+                        post = Post.find_by "id = ? AND subforum = ?", post_id, subforum_id
+
+                        forum = SubForum.find subforum_id
+                        forum_posts = JSON.parse forum.posts
+                        forum_posts.delete post.id
+                        forum.posts = forum_posts.to_json
+                        forum.save!
+
+                        post.destroy
+                    rescue ActiveRecord::RecordNotFound
+                        status = :not_found
+                    end
+                end
+
+                [status, data]
+            }
+        end
+        render status: status
+    end
+
+    def upvote_post
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::USER_CAN_UPVOTE) { |status, params, session, user|
+            begin
+                post = Post.find params[:post]
+            rescue ActiveRecord::RecordNotFound
+                status = :not_found
+            else
+                post.upvotes += 1
+                post.save!
+
+                creator = User.find_by steamID: post.creatorSteamID
+
+                if creator && creator.steamID != user.steamID
+                    creator.karma += 1
+                    creator.save!
+                end
+            end
+            [status, data]
+        }
+        render status: status
+    end
+
+    def downvote_post
+        status, data = ApplicationController.check_rights(params, session, UserGroup::RightFlags::USER_CAN_UPVOTE) { |status, params, session, user|
+            begin
+                post = Post.find params[:post]
+            rescue ActiveRecord::RecordNotFound
+                status = :not_found
+            else
+                post.downvotes += 1
+                post.save!
+
+                creator = User.find_by steamID: post.creatorSteamID
+
+                if creator && creator.steamID != user.steamID
+                    creator.karma -= 1
+                    creator.save!
+                end
+            end
+            [status, data]
+        }
+        render status: status
+    end
 end
