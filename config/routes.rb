7ae90b89c61f90a87698076ab8249703cae4ff23
pBinarySoul <pBinarySoul@gmail.com>
2020-01-25 16:00:56 +0300

    Added routes for comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  root "application#stub"
   resources :sub_forum
   get "/sub_forum/:id/posts(.:format)" => "sub_forum#show_subforum_posts"
-  get "/sub_forum/:id/posts/:post" => "sub_forum#show_post"
-  post "/sub_forum/:id/posts/create(.:format)" => "sub_forum#create_post"
-  post "/sub_forum/:id/posts/delete/:post(.:format)" => "sub_forum#destroy_post"
-  post "/sub_forum/:id/posts/upvote/:post" => "sub_forum#upvote_post"
-  post "/sub_forum/:id/posts/downvote/:post" => "sub_forum#downvote_post"
+  get "/sub_forum/:id/post/:post" => "sub_forum#show_post"
+  post "/sub_forum/:id/post/create(.:format)" => "sub_forum#create_post"
+  post "/sub_forum/:id/post/:post/delete" => "sub_forum#destroy_post"
+  post "/sub_forum/:id/post/:post/upvote" => "sub_forum#upvote_post"
+  post "/sub_forum/:id/post/:post/downvote" => "sub_forum#downvote_post"
 
-  root "application#stub"
+  get "/sub_forum/:id/post/:post/comments(.:format)" => "sub_forum#show_post_comments"
+  get "/sub_forum/:id/post/:post/comment/:comment" => "sub_forum#show_comment"
+  post "/sub_forum/:id/post/:post/comment/create(.:format)" => "sub_forum#create_comment"
+  post "/sub_forum/:id/post/:post/comment/:comment/delete" => "sub_forum#destroy_comment"
+  post "/sub_forum/:id/post/:post/comment/:comment/upvote" => "sub_forum#upvote_comment"
+  post "/sub_forum/:id/post/:post/comment/:comment/downvote" => "sub_forum#downvote_comment"
 
   post "/auth/steam/callback" => "user#create"
   post "/logout" => "user#destroy_session"
